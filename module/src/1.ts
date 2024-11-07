f47bf685b653a9a6056af9231a116cfc1871afa7
houmayan <alichy101@gmail.com>
2024-11-07 12:08:31 +0100

    first problem solved

--- module/src/1.ts
+++ module/src/1.ts
@@ -1,1 +1,12 @@
-console.log("object");
+// sum of array
+
+const sumOfArray = (num: number[]): number => {
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum = sum + num[i];
+  }
+  return sum;
+};
+
+const result = sumOfArray([1, 2, 3, 4, 5, 6]);
+console.log(result);
